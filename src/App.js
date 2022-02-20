bdadbd7338425c665ba5304b8693de2f6757678b
Ross Snyder <snyderr@oregonstate.edu>
2022-02-19 16:52:33 -0800

    elevated state out of navbar. dom restructure on home vs content page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, useLayoutEffect} from "react";
 /*import { unstable_concurrentAct } from 'react-dom/cjs/react-dom-test-utils.production.min'; */
 import './index.css';
+//CONTENT TEMPLATES
 
+//MAIN PAGE TEMPLATE CODE BELOW
 function Button(props) {
+  const [status, setStatus] = useState("gen-button"); 
+
+  function onHover(e){
+    setStatus("hover-button")
+  }
+  function exitHover(e){
+    setStatus("gen-button")
+  }
+
   return (
-    <button className="gen-button" onClick={props.clickEvent} value={props.val} onMouseEnter={props.onHover} onMouseLeave={props.exitHover}>
+    <button className={status} onClick={props.clickEvent} value={props.val} onMouseEnter={onHover} onMouseLeave={exitHover}>
       {props.val}
     </button>
   );
 }
 
 function NavBar(props){
-
+  
   const current = props.navigation;
   function renderButton(i, id, event, interact, exit) {
     return (
@@ -24,30 +35,18 @@ function NavBar(props){
     );
   }
 
-  function onHover(e){
-    e.target.style.background = 'rgba(6, 28, 33, .5)';
-    e.target.style.transform = 'scale(1.1, 1)';
-    e.target.style.transition= 'all 0.6s ease';
-  }
-  function exitHover(e){
-    e.target.style.color = 'white';
-    e.target.style.background = '#ffffff00';
-    e.target.style.transform = 'scale(1, 1)';
-    e.target.style.transition= 'all 0.1s ease';
-    
-  }
-
-  function handleClick(e){
+  function buttonClick(e){
+    //console.log("button click ", e.target.value);
+    //setVisibility(false);
     props.changePage(e.target.value);
   }
 
   return(
-    
-      <div className="nav-bar">
-        {current.map((button, index) => 
-        renderButton(current[index], index, handleClick, onHover, exitHover))}
-      </div>
-    );
+    <div className={props.visibility}>
+      {current.map((button, index) => 
+      renderButton(current[index], index, buttonClick))}
+    </div>
+  );
 }
 
 function Content(props){
@@ -59,34 +58,57 @@ function Content(props){
 }
 
 function Page(props) {
+  const now = useRef("HOME");
   const initialState = {
-    title: "Ross Snyder",
-    navigation: ["HOME", "PROJECTS", "ABOUT", "MORE"]
+    title: "HOME",
+    navigation: ["HOME", "PROJECTS", "ABOUT", "MORE"],
+    navVis: true
   }
   const [state, setState] = useState(initialState);
   
-
   function changePage(nTitle){
     const old = state;
-    if(nTitle === "Home"){
-      nTitle = "Ross Snyder";
+    if(nTitle === "HOME"){
+      setState({
+        title: nTitle,
+        navigation: old.navigation,
+        navVis: true
+      });
+    } else {
+      setState({
+        title: nTitle,
+        navigation: old.navigation,
+        navVis: false
+      });
     }
-    setState({
-      title: nTitle,
-      navigation: old.navigation
-    });
+    console.log("page changed");
   }
-
+  if(state.navVis != true){
     return (
         <div className="Page">
           <h1>{state.title}</h1>
           <NavBar 
           navigation={state.navigation}
           changePage={changePage}
+          visibility={"hid-bar"}
           /> 
           <Content source={state.title + ".jpg"} alt={state.title}/>
         </div>
     );
+  } else {
+    return (
+      <div className="Page">
+        <h1>{state.title}</h1>
+        <Content source={state.title + ".jpg"} alt={state.title}/>
+        <NavBar 
+        navigation={state.navigation}
+        changePage={changePage}
+        visibility={"nav-bar"}
+        /> 
+      </div>
+  );
+
+    }
 }
 
 function App() {
