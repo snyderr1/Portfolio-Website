cc1a773d975cb0326866c03956e7a1fcdc262e21
Ross Snyder <snyderr@oregonstate.edu>
2022-03-02 12:13:38 -0800

    adding images, small refactoring on buttons. starting on content

--- src/App.js
+++ src/App.js
@@ -1,121 +1,176 @@
 import React, { useEffect, useState, useRef, useLayoutEffect} from "react";
 /*import { unstable_concurrentAct } from 'react-dom/cjs/react-dom-test-utils.production.min'; */
 import './index.css';
-//CONTENT TEMPLATES
+//Author: Ross Snyder
+//2021
 
-//MAIN PAGE TEMPLATE CODE BELOW
+//buttons
 function Button(props) {
   const [status, setStatus] = useState("gen-button"); 
-
+  //status denotes the button type for styling. hover causees button to expand.
   function onHover(e){
     setStatus("hover-button")
   }
+  //resets the button on mouse exit.
   function exitHover(e){
     setStatus("gen-button")
   }
 
   return (
-    <button className={status} onClick={props.clickEvent} value={props.val} onMouseEnter={onHover} onMouseLeave={exitHover}>
-      {props.val}
+    <button className={status} value={props.type} onClick={props.clickEvent} onMouseEnter={onHover} onMouseLeave={exitHover}>
+      {props.type}
     </button>
   );
 }
-
+//navigation bar
 function NavBar(props){
-  
-  const current = props.navigation;
-  function renderButton(name, event, interact, exit) {
-    return (
-      <Button
-        val={name} 
-        clickEvent={event}
-        onHover={interact}
-        exitHover={exit}
-      />
-    );
-  }
 
   function buttonClick(e){
-    //console.log("button click ", e.target.value);
-    //setVisibility(false);
+    //change to the page to the buttons value, ie the new page name
+    console.log("target value is:", e.target);
     props.changePage(e.target.value);
+    
   }
-
-  return(
-    <div className={props.visibility}>
-      {props.navigation.map((button) => 
-      renderButton(button, buttonClick))}
-    </div>
-  );
+  //if this navbar is set to be shown on the page:
+  if(props.shown) {
+    return(
+      <div className={props.type}>
+        {props.navigation.map((name) => 
+        <Button key={name} type={name} clickEvent={buttonClick} />)}
+      </div>
+    );
+  } else {
+    //otherwise render nothing (or rather, a hidden div)
+    return(
+      <div className="hid-bar">
+      </div>
+    );
+  }
 }
 
 function Content(props){
-  let def = "page-content";
-  if(props.type === "HOME"){
-    def = "home-content"
+  //render the correct page content based on current title
+  if(props.type === "Home") {
+    return (
+      <Home />
+    );
+  } else if(props.type === "Projects") {
+    return (
+      <Projects />
+    );
+  } else if(props.type === "Resume") {
+    return (
+      <Resume />
+    );
+  } else if(props.type === "Whiteboard") {
+    return (
+      <Whiteboard />
+    );
+  } else {
+    //this should spit out an error ideally. only those 4 are in use.
+    return (
+      <Home />
+    );
   }
+
+  console.log(props.type);
+}
+
+//prints content onto pages.
+function Home(props){
+  //def is the name of the content, so that the css can be properly applied
+  //for different pages and content types
   return (
-    <div className= {def}>
+    <div className= "Home">
       <div className="page-window">
-        <img className="mainimg" src={props.source} alt={props.alt}></img>
+      <img className="portrait" src="me.jpg" alt={props.alt}></img>
       </div>
     </div>
   );
 }
-
+function Projects(props){
+  //def is the name of the content, so that the css can be properly applied
+  //for different pages and content types
+  return (
+    <div className= "Projects">
+      <div className="page-window">
+        
+      </div>
+    </div>
+  );
+}
+function Resume(props){
+  //def is the name of the content, so that the css can be properly applied
+  //for different pages and content types
+  return (
+    <div className= "Resume">
+      <div className="page-window">
+        
+      </div>
+    </div>
+  );
+}
+function Whiteboard(props){
+  //def is the name of the content, so that the css can be properly applied
+  //for different pages and content types
+  return (
+    <div className= "Whiteboard">
+      <div className="page-window">
+        <img className="board" src={props.source} alt={props.alt}></img>
+      </div>
+    </div>
+  );
+}
+//the full page
 function Page(props) {
-  const now = useRef("HOME");
+  const now = useRef("Home");
+  //default is home, also setting the default page names and bar position to be passed to children
   const initialState = {
-    title: "HOME",
-    navigation: ["HOME", "PROJECTS", "ABOUT", "MORE"],
-    navVis: "nav-bar"
+    title: "Home",
+    navigation: ["Home", "Projects", "Resume", "Whiteboard"],
+    barPos: "bottom"
   }
   const [state, setState] = useState(initialState);
   
   function changePage(nTitle){
     const old = state;
-    if(nTitle === "HOME"){
+    //set the navbar to the bottom of the page while at home.
+    //to emphasis my face.
+    if(nTitle === "Home"){
       setState({
         title: nTitle,
         navigation: old.navigation,
-        navVis: "nav-bar"
+        barPos: "bottom"
       });
     } else {
       setState({
         title: nTitle,
         navigation: old.navigation,
-        navVis: "hid-bar"
+        barPos: "top"
       });
     }
     console.log("page changed");
   }
   //messy fix this vvvv
-  if(state.navVis != "nav-bar"){
-    return (
-        <div className="Page">
-          <h1>{state.title}</h1>
-          <NavBar 
-          navigation={state.navigation}
-          changePage={changePage}
-          visibility={state.navVis}
-          /> 
-          <Content source={state.title + ".jpg"} alt={state.title} type={state.title}/>
-        </div>
-    );
-  } else {
-    return (
+  //2 navbars instead of having two seperate hooks or a homebar and contentbar
+  return (
       <div className="Page">
         <h1>{state.title}</h1>
-        <Content source={state.title + ".jpg"} alt={state.title} type = {state.title}/>
         <NavBar 
         navigation={state.navigation}
         changePage={changePage}
-        visibility={state.navVis}
+        shown={(state.title !== "Home" && state.barPos ==="top") ? true : false}
+        type={"nav-bar"}
+        /> 
+        <Content type={state.title}/>
+        <NavBar 
+        navigation={state.navigation}
+        changePage={changePage}
+        shown={(state.title === "Home" && state.barPos ==="bottom") ? true : false}
+        type={"home-bar"}
         /> 
       </div>
   );
-
-  }
 }
 
 
