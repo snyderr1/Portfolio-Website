f5c34a36ff05961d6ade912713d2ee5d118dc49a
Ross Snyder <rossesny@gmail.com>
2022-10-11 21:21:43 -0700

    refactored routing finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React, { useEffect, useState, useRef, useLayoutEffect} from "react";
-//import {Routes, Route, useNavigate} from 'react-router-dom';
+import React, { useEffect, useState, useRef, useLayoutEffect, Fragment} from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+ } from "react-router-dom";
 /*import { unstable_concurrentAct } from 'react-dom/cjs/react-dom-test-utils.production.min'; */
 import './index.css';
 import Me from './me.jpg';
@@ -41,9 +46,9 @@ function Button(props) {
   }
 
   return (
-    <button className={status} value={props.type} onClick={props.clickEvent} onMouseEnter={onHover} onMouseLeave={exitHover}>
-      {props.type}
-    </button>
+      <button className={status} value={props.type} onMouseEnter={onHover} onMouseLeave={exitHover}>
+        {props.type}
+      </button>
   );
 }
 //navigation bar
@@ -56,14 +61,6 @@ function NavBar(props){
   );
 }
 
-function Content(props){
-  return(
-    <div className="Home"></div>
-  );
-  
-
-  console.log(props.type);
-}
 
 //prints content onto pages.
 function Home(props){
@@ -76,8 +73,7 @@ function Home(props){
         <div className="home-text">         
           <p className ="home-top-blurb">Welcome! My name is Ross Snyder, and this is my website. I recieved my degree in Computer Science Systems
           in 2021 from Oregon State University. </p>
-          <p className = "home-bottom-blurb">I am looking for employment as a web developer or software engineer. 
-          Click the navigation bar or links below to checkout my current projects, previous experience, and more.</p>
+          <p className = "home-bottom-blurb">Text</p>
           <div className = "home-links">
             <ImgLink name="git" src={GitIcon} link="https://github.com/snyderr1/" linkText="https://github.com/snyderr1" />
             <ImgLink name="email" src={EmailIcon} link="rossesny@gmail.com" linkText="rossesny@gmail.com" />
@@ -89,7 +85,7 @@ function Home(props){
     </div>
   );
 }
-/* function Resume(props){
+function Resume(props){
   //def is the name of the content, so that the css can be properly applied
   //for different pages and content types
   return (
@@ -107,7 +103,7 @@ function Projects(props){
     <div className= "Projects">
       <div className="project-container">
         {Object.entries(projectInfo).map((info) => 
-        <GenericText title={info[0]} text={info[1]}/>)}
+        <GenericText key = {info[0]} title={info[0]} text={info[1]}/>)}
       </div>
     </div>
   );
@@ -122,27 +118,13 @@ function About(props){
       </div>
     </div>
   );
-} */
+} 
 //the full page
 function Page(props) {
-  
-  //default is home, also setting the default page names and bar position to be passed to children
-  //const now = useRef("Home");
-  return (
-    <div className="Page">
-      {props.children}
 
-    </div>
-  );
-}
-
-
-function App() {
-
-  
   const initialState = {
     title: "Home",
-    navigation: ["Home", "Resume", "Projects", "About Me"]
+    navigation: ["Home", "Resume", "Projects", "About"]
     //barPos: "bottom"
   }
 
@@ -153,18 +135,38 @@ function App() {
       title: nTitle,
       navigation: old.navigation
     });
+    console.log(nTitle)
   }
-  
+  //default is home, also setting the default page names and bar position to be passed to children
+  //const now = useRef("Home");
+  return (
+    <div className="Page">
+      <NavBar>
+        {state.navigation.map((name) => {
+        return <><Link to={"/" + name} ><Button  type={name} /></Link></>
+        })}
+      </NavBar>
+      <div className = "Content">{props.children}</div>
+    </div>
+  );
+}
+
+
+function App() {
+
   
   return (
-    <Page >
-    <NavBar>
-      {state.navigation.map((name) => 
-      <Button key={name} type={name} clickEvent={setTitle} />)}
-    </NavBar>
-      <Content type={state.title}/>
-    </Page>
-    
+    <div className ="App">
+      <Router>
+        <Routes>
+          <Route index element={<Page><Home/></Page>} />
+          <Route path="/Home" element={<Page><Home/></Page>} />
+          <Route path="/Projects" element={<Page><Projects/></Page>} />
+          <Route path="/Resume" element={<Page><Resume/></Page>} />
+          <Route path="/About" element={<Page><About/></Page>} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
